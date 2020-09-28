a9d2b32350b7e4141cfdff5cfd701a5ea7cb53f6
Meepit <jwlizamore@gmail.com>
2020-09-28 04:37:01 +0100

    Fix time commands

--- utils.js
+++ utils.js
@@ -1,6 +1,17 @@
 const moment = require('moment-timezone');
 
 function timeConversion(args) {
+	if (args.length == 1) {
+		const panama = moment.tz('America/Panama');
+		const london = panama.clone().tz('Europe/London');
+		const germany = panama.clone().tz('Europe/Berlin');
+		let message = '';
+		message += (`The time in Panama is ${panama.format('HH:mm')}\n`);
+		message += (`The time in the UK is ${london.format('HH:mm')}\n`);
+		message += (`The time in Germany is ${germany.format('HH:mm')}\n`);
+		return message;
+	}
+
 	if (args.length == 2) {
 		const baseCountry = args[1].toLowerCase();
 		if (baseCountry === 'panama') {
@@ -13,11 +24,32 @@ function timeConversion(args) {
 			return(`${moment.tz('Europe/Berlin').format('HH:mm')}`);
 		}
 	}
+
 	if (args.length === 3) {
+		console.log(args);
 		const time = args[2];
-		const panama = moment.tz(time, 'HHmm', 'America/Panama');
-		const london = panama.clone().tz('Europe/London');
-		const germany = panama.clone().tz('Europe/Berlin');
+		const country = args[1];
+		let panama = '';
+		let london = '';
+		let germany = '';
+		console.log(country);
+		if (country === 'panama') {
+			panama = moment.tz(time, 'HHmm', 'America/Panama');
+			london = panama.clone().tz('Europe/London');
+			germany = panama.clone().tz('Europe/Berlin');
+		}
+		else if (country === 'uk') {
+			london = moment.tz(time, 'HHmm', 'Europe/London');
+			panama = london.clone().tz('America/Panama');
+			germany = london.clone().tz('Europe/Berlin');
+		}
+		else if (country === 'germany') {
+			germany = moment.tz(time, 'HHmm', 'Europe/Berlin');
+			london = germany.clone().tz('Europe/London');
+			panama = germany.clone().tz('America/Panama');
+		} else {
+			return '';
+		}
 		let message = '';
 		message += (`The time in Panama is ${panama.format('HH:mm')} \n`);
 		message += (`The time in the UK is ${london.format('HH:mm')}\n`);
