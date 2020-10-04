5fc2a9a4356ad9c6608585b1527dd5632bfe9704
Meepit <jwlizamore@gmail.com>
2020-10-04 15:35:38 +0100

    Complete airport feature, add airports data csv

--- PhebyBot.js
+++ PhebyBot.js
@@ -5,10 +5,34 @@ const { PREFIX, API_KEY } = require('./config');
 const utils = require('./utils');
 const templates = require('./templates/templates.js');
 const ThankQuotes = require('./templates/ThankYouQuotes.js');
+const csv = require('csv-parser');
+const fs = require('fs');
+const geolib = require('geolib');
+// import { getDistance } from 'geolib';
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+
+const data_arr = [];
+fs.createReadStream('airports_msfs2020_pheby.csv')
+	.pipe(csv())
+	.on('data', function(data) {
+		try {
+			// perform the operation
+			data_arr.push(data);
+		}
+		catch(err) {
+			// error handler
+			console.log(err);
+		}
+	})
+	.on('end', function() {
+		console.log('done');
+	});
+
 
 client.on('message', message => {
 	// No reason to respond to other bots or myself
@@ -36,6 +60,55 @@ client.on('message', message => {
 	if (message.content === `${PREFIX}thanks`) {
 		console.log(ThankQuotes.RanThanks);
 		message.channel.send(_.sample(ThankQuotes.RanThanks));
+	}
+
+	if (message.content.startsWith(`${PREFIX}flight`)) {
+		// -flight icon 42
+		const iconSpeed = 193;
+		const requestedTime = parseInt(args[2]) / 60;
+		const randAirport = _.sample(data_arr);
+		let filteredAirports = [];
+		for (const airport of data_arr) {
+			const dist = geolib.getDistance(
+				{ latitude: randAirport.laty, longitude: randAirport.lonx },
+				{ latitude: airport.laty, longitude:airport.lonx },
+			) / 1000;
+			const flightTime = dist / iconSpeed;
+			if (flightTime <= requestedTime) {
+				airport.flight_time = flightTime;
+				filteredAirports.push(airport);
+			}
+		}
+		const destAirport = filteredAirports.pop();
+		filteredAirports = filteredAirports.sort(utils.flightTimeCompare);
+		const depMsg = templates.FlightPlanMsg(
+			'Departure',
+			randAirport.name,
+			randAirport.laty,
+			randAirport.lonx,
+			message.author.username,
+			randAirport.ident,
+			randAirport.city,
+			utils.feetToMeters(randAirport.longest_runway_length),
+			utils.feetToMeters(randAirport.longest_runway_width),
+			utils.feetToMeters(randAirport.altitude),
+			randAirport.tower_frequency,
+		);
+		const arrMsg = templates.FlightPlanMsg(
+			'Arrival',
+			destAirport.name,
+			destAirport.laty,
+			destAirport.lonx,
+			message.author.username,
+			destAirport.ident,
+			destAirport.city,
+			utils.feetToMeters(destAirport.longest_runway_length),
+			utils.feetToMeters(destAirport.longest_runway_width),
+			utils.feetToMeters(destAirport.altitude),
+			destAirport.tower_frequency,
+		);
+		message.channel.send(depMsg);
+		message.channel.send(arrMsg);
 	}
 });
 
--- utils.js
+++ utils.js
@@ -32,39 +32,53 @@ function timeConversion(args) {
 		let panama = '';
 		let london = '';
 		let germany = '';
+		let message = '';
 		console.log(country);
 		if (country === 'panama') {
 			panama = moment.tz(time, 'HHmm', 'America/Panama');
 			london = panama.clone().tz('Europe/London');
 			germany = panama.clone().tz('Europe/Berlin');
+			message += (`The time in the UK is ${london.format('HH:mm')}\n`);
+			message += (`The time in Germany is ${germany.format('HH:mm')}\n`);
 		}
 		else if (country === 'uk') {
 			london = moment.tz(time, 'HHmm', 'Europe/London');
 			panama = london.clone().tz('America/Panama');
 			germany = london.clone().tz('Europe/Berlin');
+			message += (`The time in Panama is ${panama.format('HH:mm')} \n`);
+			message += (`The time in Germany is ${germany.format('HH:mm')}\n`);
 		}
 		else if (country === 'germany') {
 			germany = moment.tz(time, 'HHmm', 'Europe/Berlin');
 			london = germany.clone().tz('Europe/London');
 			panama = germany.clone().tz('America/Panama');
+			message += (`The time in Panama is ${panama.format('HH:mm')} \n`);
+			message += (`The time in the UK is ${london.format('HH:mm')}\n`);
 		}
 		else {
 			return '';
 		}
-		let message = '';
-		message += (`The time in Panama is ${panama.format('HH:mm')} \n`);
-		message += (`The time in the UK is ${london.format('HH:mm')}\n`);
-		message += (`The time in Germany is ${germany.format('HH:mm')}\n`);
 		return message;
 	}
 }
 
+
+function flightTimeCompare(a, b) {
+	if (a.flight_time < b.flight_time) {
+	  return -1;
+	}
+	if (a.flight_time > b.flight_time) {
+	  return 1;
+	}
+	return 0;
+}
+
+function feetToMeters(feet) {
+	return parseInt(feet / 3.28);
+}
+
 module.exports = {
 	timeConversion,
+	flightTimeCompare,
+	feetToMeters,
 };
-
-
-// {
-//     "Mexico": "America/Mexico",
-//     "United Kingdom": "Europe/London"
-// }
